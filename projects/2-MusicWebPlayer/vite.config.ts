508c9a6498a15ca422c46ab809cbc370934a4f05
Fabián González <alu0101549491@ull.edu.es>
2026-01-08 19:11:15 +0000

    FIX: 2. Music Web Player - vite base route fix

--- projects/2-MusicWebPlayer/vite.config.ts
+++ projects/2-MusicWebPlayer/vite.config.ts
@@ -1,3 +1,17 @@
+/**
+ * University of La Laguna
+ * School of Engineering and Technology
+ * Degree in Computer Engineering
+ * Final Degree Project (TFG)
+ *
+ * @author Fabián González Lence <alu0101549491@ull.edu.es>
+ * @since January 9, 2026
+ * @file TFG-Fabian-Gonzalez-Lence/projects/2-MusicWebPlayer/vite.config.ts
+ * @desc Vite configuration for development and build.
+ * @see {@link https://github.com/alu0101549491/TFG-Fabian-Gonzalez-Lence/tree/main/projects/2-MusicWebPlayer}
+ * @see {@link https://typescripttutorial.net}
+ */
+
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import {resolve} from 'path';
@@ -6,6 +20,7 @@ export default defineConfig({
   plugins: [react()],
   root: '.',
   publicDir: 'public',
+  base: '/2-MusicWebPlayer/dist/',  // KEY FIX!s
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
