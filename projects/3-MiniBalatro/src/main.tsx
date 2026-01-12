22af0fa9b2ba7dce3acbd79bc99281bd5b0cbdfa
alu0101549491 <alu0101549491@ull.edu.es>
2026-01-12 11:56:26 +0000

    FEAT: 3. Mini Balatro - Frontend phase done with the modification of the main CSS files for each component and the global styles

--- projects/3-MiniBalatro/src/main.tsx
+++ projects/3-MiniBalatro/src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './views/App';
-import './index.css';
+import '../public/assets/styles/global.css';
+import '../public/assets/styles/animations.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
