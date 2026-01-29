a5d412ad470f6c520a2b8d6e271a998ee479288b
alu0101549491 <alu0101549491@ull.edu.es>
2026-01-29 14:39:14 +0000

    FIX: 3. Mini Balatro - indexes missing

---
+++ projects/3-MiniBalatro/src/views/components/modals/index.ts
@@ -0,0 +1,18 @@
+/**
+ * University of La Laguna
+ * School of Engineering and Technology
+ * Degree in Computer Engineering
+ * Final Degree Project (TFG)
+ *
+ * @author Fabián González Lence <alu0101549491@ull.edu.es>
+ * @since January 29, 2026
+ * @file src/views/components/modals/index.ts
+ * @desc Barrel export for modals
+ * @see {@link https://github.com/alu0101549491/TFG-Fabian-Gonzalez-Lence/tree/main/projects/3-MiniBalatro}
+ * @see {@link https://typescripttutorial.net}
+ */
+
+export * from './BlindDefeatModal';
+export * from './BlindVictoryModal';
+export * from './GameVictoryModal';
+export * from './HelpModal';
